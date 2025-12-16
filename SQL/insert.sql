d804ebaecd90897f6972a4c68bc90b51725c328d
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 12:50:06 -0300

    Update insert.sql

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -18,10 +18,9 @@ INSERT INTO viagens.reservas (id, idUsuario, idDestino, dataReserva, statusReser
 (4, 3, 3, '2023-09-20', 'cancelada');
 
 -- Migrando os dados --
-
--- INSERT INTO usuarios_nova (id, nome, email, endereco, dataNascimento)
--- SELECT id, nome email, dataNascimento
--- FROM usuarios;
+INSERT INTO usuarios_nova (id, nome, email, endereco, dataNascimento)
+SELECT id, nome, email, endereco, dataNascimento
+FROM usuarios;
 
 INSERT INTO usuarios_nova
 SELECT * from usuarios;
