bc3ab0a05fc8f0bbb85234fa3de8344faa89af02
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 15:51:36 -0300

    Update consultas.js

--- NoSQL/consultas.js
+++ NoSQL/consultas.js
@@ -30,6 +30,19 @@ db.usuarios.replaceOne(
   }
 );
 
+// Update Operadores
+// Usando o operador $set para definir o valor de um campo específico
+db.usuarios.updateOne({ nome: "João" }, { $set: { idade: 26 } });
+
+// Usando o operador $inc para incrementar o valor de um campo numérico
+db.usuarios.updateOne({ nome: "João" }, { $inc: { idade: 1 } });
+
+// Usando o operador $rename para renomear um campo existente
+db.usuarios.updateOne({ nome: "João" }, { $rename: { "endereco.rua": "endereco.nomeRua" } });
+
+// Usando o operador $unset para remover um campo específico de um documento
+db.usuarios.updateOne({ nome: "João" }, { $unset: { endereco: "" } });
+
 // Delete
 db.usuarios.deleteOne({nome: "João"}); // Exclui o 1o que achar
 db.usuarios.deleteMany({nome: "Pamela"}); // Exclui todos
