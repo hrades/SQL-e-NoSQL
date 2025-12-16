a5c46d51335d6f880805d81aa01851d02522a364
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 12:23:26 -0300

    Create select.sql

---
+++ SQL/select.sql
@@ -0,0 +1,11 @@
+-- Selecionar todos os registros da tabela "usuarios"
+SELECT * FROM usuarios;
+
+-- Selecionar apenas o nome e o email dos usuários
+SELECT nome, email FROM usuarios;
+
+-- Selecionar os usuários que possuem o nome "João Silva"
+SELECT * FROM usuarios WHERE nome = 'João Silva';
+
+-- Selecionar os usuários que nasceram antes de uma determinada data
+SELECT * FROM usuarios WHERE data_nascimento < '1990-01-01';
