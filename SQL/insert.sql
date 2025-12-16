67807958975acf0e079f86e8c2a93fe48d73a334
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 12:57:43 -0300

    Update insert.sql

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -24,3 +24,15 @@ FROM usuarios;
 
 INSERT INTO usuarios_nova
 SELECT * from usuarios;
+
+-- Inserção na tabela "usuarios"
+INSERT INTO usuarios (nome, email, data_nascimento, endereco)
+VALUES ('João Maria', 'joaomaria@example.com', '1990-01-01', 'Rua A, 123');
+
+-- Inserção na tabela "destinos"
+INSERT INTO destinos (nome, descricao)
+VALUES ('Praia Teste', 'Destino paradisíaco com belas praias.');
+
+-- Inserção na tabela "reservas"
+INSERT INTO reservas (id_usuario, id_destino, data, status)
+VALUES (4, 4, '2023-07-01', 'pendente');
