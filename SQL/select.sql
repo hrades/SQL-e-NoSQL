7825780c44a79434b52c8ec97592ecbdf03296ab
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 12:28:02 -0300

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -9,3 +9,10 @@ SELECT * FROM usuarios WHERE nome = 'João Silva';
 
 -- Selecionar os usuários que nasceram antes de uma determinada data
 SELECT * FROM usuarios WHERE data_nascimento < '1990-01-01';
+
+-- Selecionar os usuarios em que o id for maior que 1 e o nome contiver Maria
+SELECT * FROM usuarios WHERE id > 1 AND nome LIKE "%Maria%";
+
+-- Like
+SELECT * FROM usuarios WHERE nome LIKE '%Silva%';
+SELECT * FROM usuarios WHERE nome LIKE 'Jo_o%';
