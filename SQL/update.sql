67794d5a2075d80b26fd946ae6f7f5311a6d72c9
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2025-12-16 13:35:32 -0300

    Update update.sql

--- SQL/update.sql
+++ SQL/update.sql
@@ -5,3 +5,11 @@ UPDATE usuarios
 UPDATE usuarios
 	SET nome = "Ana Oliveira"
   WHERE id = 1;
+
+-- Normalização --
+-- Copia os dados da tabela original para a nova tabela
+UPDATE usuarios
+SET rua = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 1), ',', -1),
+    numero = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 2), ',', -1),
+    cidade = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 3), ',', -1),
+    estado = SUBSTRING_INDEX(endereco, ',', -1);
